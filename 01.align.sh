c853a8204ec6e01f7ec7fdafe4b01785aab90ccb
Marne Quigg <m.quigg1475@gmail.com>
2025-11-25 11:22:50 -0500

    Update 01.align.sh
    Made a new script with the execute instructions

--- 01.align.sh
+++ 01.align.sh
@@ -77,25 +77,4 @@ status "SUCCESS" "All steps completed"
 #update permissions so you can execute it
 chmod +x align_samples.sh
 
-#here you have two options: you can either run the script with ./align_samples.sh or you can use the script below to run multiple samples at once
-
-#open a new nano
-nano execute_samples.sh
-
-###copy this into the nano###
-#!/bin/bash
-
-# Sample list based on paired R1 files
-SAMPLE_LIST=$(ls /data/labs/Fant/Quigg/batch_3_missing_processing/01.trimmed/*_R1_paired.fastq.gz | sed 's|.*/||; s/_R1_paired.fastq.gz//' | sort -u)
-
-# Run 5 samples in parallel
-echo "$SAMPLE_LIST" | parallel -j 5 ./align_samples.sh
-
-###
-
-#exit
-chmod +x execute_samples.sh
-
-cd /data/labs/Fant/Quigg/03c.alignment_diseases+elm/redo_missing_samples
-mkdir delete
-mv *.sam /data/labs/Fant/Quigg/03c.alignment_diseases+elm/redo_missing_samples/delete
+#here you have two options: you can either run the script with ./align_samples.sh or you can use the script 01b.execute_alignment.sh to run multiple samples at once
